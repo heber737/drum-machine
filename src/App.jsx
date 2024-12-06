83eec80fde7d2242737fc9fe68f550bbb30bcf85
Heber Villalobos <hebervillalobos@gmail.com>
2024-12-06 15:22:24 -0500

    modified:   index.html 	modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/banks.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
+import { bankOne, bankTwo } from "./banks.js";
 
 export default function App() {
   const [power, setPower] = useState(true);
@@ -8,7 +9,7 @@ export default function App() {
   const [clicked, setClicked] = useState(null);
   const [volume, setVolume] = useState(0.5);
 
-  function tooglePower() {
+  function handleTooglePower() {
     if (power === true) {
       setPower(false);
     } else {
@@ -29,35 +30,9 @@ export default function App() {
   }
 
   function handleVolume(e) {
-    setVolume(e.target.value)
+    setVolume(e.target.value);
   }
 
-  return (
-    <div id="main">
-      <DrumMachine
-        power={power}
-        tooglePower={tooglePower}
-        bank={bank}
-        toogleBank={toogleBank}
-        clicked={clicked}
-        handleClick={handleClick}
-        volume={volume}
-        handleVolume={handleVolume}
-      />
-    </div>
-  );
-}
-
-function DrumMachine({
-  power,
-  tooglePower,
-  bank,
-  toogleBank,
-  clicked,
-  handleClick,
-  volume,
-  handleVolume
-}) {
   return (
     <div id="drum-machine">
       <div id="drum-set-wrapper">
@@ -66,7 +41,7 @@ function DrumMachine({
       <div id="controllers">
         <Controllers
           power={power}
-          tooglePower={tooglePower}
+          onTooglePower={handleTooglePower}
           bank={bank}
           toogleBank={toogleBank}
           clicked={clicked}
@@ -80,21 +55,25 @@ function DrumMachine({
 
 function DrumSet({ handleClick, bank, volume }) {
   return (
-    <div id="drum-set">
-      <DrumPad innerText="Q" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="W" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="E" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="A" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="S" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="D" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="Z" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="X" handleClick={handleClick} bank={bank} volume={volume}/>
-      <DrumPad innerText="C" handleClick={handleClick} bank={bank} volume={volume}/>
-    </div>
+    <ul id="drum-set">
+      {bank.map((drumpad, index) => {
+        return (
+          <li className="drumpad-list" key={drumpad.key}>
+            <DrumPad
+              index={index}
+              handleClick={handleClick}
+              bank={bank}
+              volume={volume}
+              name={drumpad.name}
+            />
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
-function DrumPad({ innerText, handleClick, bank, volume }) {
+function DrumPad({ index, handleClick, bank, volume, name }) {
   const acceptedKeys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
   document.addEventListener("keydown", (e) => {
     let pressed = e.code.slice(3);
@@ -109,17 +88,17 @@ function DrumPad({ innerText, handleClick, bank, volume }) {
     <>
       <button
         className="drum-pad"
-        id={innerText + "input"}
+        id={name + "input"}
         onClick={(e) => {
           handleClick(e);
-          let currentAudio = document.getElementById(innerText);
+          let currentAudio = document.getElementById(name);
           if (
             currentAudio.paused == false &&
             currentAudio.paused == false &&
             currentAudio.currentTime > 0
           ) {
             currentAudio.src = "";
-            currentAudio.src = bank[innerText].source;
+            currentAudio.src = bank[index].source;
           }
           currentAudio.volume = volume;
           currentAudio.play();
@@ -129,24 +108,32 @@ function DrumPad({ innerText, handleClick, bank, volume }) {
           }, 150);
         }}
       >
-        {innerText}
+        {name}
         <audio
           className="clip"
-          src={bank[innerText].source}
-          id={innerText}
+          src={bank[index].source}
+          id={name}
         ></audio>
       </button>
     </>
   );
 }
 
-function Controllers({ power, tooglePower, bank, toogleBank, clicked, volume, handleVolume }) {
+function Controllers({
+  power,
+  onTooglePower,
+  bank,
+  toogleBank,
+  clicked,
+  volume,
+  handleVolume,
+}) {
   return (
     <>
       <Display clicked={clicked} />
-      <Power power={power} tooglePower={tooglePower} />
+      <Power power={power} onTooglePower={onTooglePower} />
       <Bank bank={bank} toogleBank={toogleBank} />
-      <Volume  volume={volume} handleVolume={handleVolume}/>
+      <Volume volume={volume} handleVolume={handleVolume} />
     </>
   );
 }
@@ -155,13 +142,13 @@ function Display({ clicked }) {
   return <div id="display">{clicked}</div>;
 }
 
-function Power({ power, tooglePower }) {
+function Power({ power, onTooglePower }) {
   return (
     <div id="power">
       Power
       <div
         id="on-off"
-        onClick={tooglePower}
+        onClick={onTooglePower}
         style={
           power ? { backgroundColor: "green" } : { backgroundColor: "red" }
         }
@@ -184,85 +171,15 @@ function Bank({ bank, toogleBank }) {
 function Volume({ volume, handleVolume }) {
   return (
     <div id="volume">
-      <input id="slider" type="range" min="0" max="1" step="0.1" value={volume} onChange={(e) => handleVolume(e)}/>
+      <input
+        id="slider"
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        value={volume}
+        onChange={(e) => handleVolume(e)}
+      />
     </div>
   );
 }
-
-const bankOne = {
-  Q: {
-    name: "Heater 1",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-  },
-  W: {
-    name: "Heater 2",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-  },
-  E: {
-    name: "Heater 3",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-  },
-  A: {
-    name: "Heater 4",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-  },
-  S: {
-    name: "Clap",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  },
-  D: {
-    name: "Open-HH",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  },
-  Z: {
-    name: "Kick-n'-Hat",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  },
-  X: {
-    name: "Kick",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  },
-  C: {
-    name: "Closed-HH",
-    source: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-  },
-};
-
-const bankTwo = {
-  Q: {
-    name: "C",
-    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key08.mp3",
-  },
-  W: {
-    name: "D",
-    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key10.mp3",
-  },
-  E: {
-    name: "E",
-    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key12.mp3",
-  },
-  A: {
-    name: "F",
-    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key13.mp3",
-  },
-  S: {
-    name: "G",
-    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key15.mp3",
-  },
-  D: {
-    name: "A",
-    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key17.mp3",
-  },
-  Z: {
-    name: "B",
-    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key19.mp3",
-  },
-  X: {
-    name: "C2",
-    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key20.mp3",
-  },
-  C: {
-    name: "D2",
-    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key22.mp3",
-  },
-};
---
+++ src/banks.js
@@ -0,0 +1,95 @@
+export const bankOne = [
+  {
+    key: "Q",
+    name: "Heater 1",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    key: "W",
+    name: "Heater 2",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    key: "E",
+    name: "Heater 3",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    key: "A",
+    name: "Heater 4",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    key: "S",
+    name: "Clap",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    key: "D",
+    name: "Open-HH",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    key: "Z",
+    name: "Kick-n'-Hat",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    key: "X",
+    name: "Kick",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    key: "C",
+    name: "Closed-HH",
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+export const bankTwo = [
+  {
+    key: "Q",
+    name: "C",
+    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key08.mp3",
+  },
+  {
+    key: "W",
+    name: "D",
+    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key10.mp3",
+  },
+  {
+    key: "E",
+    name: "E",
+    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key12.mp3",
+  },
+  {
+    key: "A",
+    name: "F",
+    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key13.mp3",
+  },
+  {
+    key: "S",
+    name: "G",
+    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key15.mp3",
+  },
+  {
+    key: "D",
+    name: "A",
+    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key17.mp3",
+  },
+  {
+    key: "Z",
+    name: "B",
+    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key19.mp3",
+  },
+  {
+    key: "X",
+    name: "C2",
+    source: "https://ia800106.us.archive.org/13/items/24-piano-keys/key20.mp3",
+  },
+  {
+    key: "C",
+    name: "D2",
+    source: "https://ia600106.us.archive.org/13/items/24-piano-keys/key22.mp3",
+  },
+];
